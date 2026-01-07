20bebcfbbcd2064776216dc6c1de852b4f2d39fb
khangtruong-ui <khang.truong@gmail.com>
2026-01-07 21:54:03 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+pip install -q jax[tpu] flax optax transformers==4.57.3 datasets diffusers==0.36 torch torchvision Pillow -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+
+DIFF_PATH=$(pip list -v | grep diffusers | awk '{print $3}')
+echo "INSTALLED DIFFUSER: $DIFF_PATH"
+
+cp pipeline_flax_stable_diffusion.py "$DIFF_PATH/diffusers/pipelines/stable_diffusion"

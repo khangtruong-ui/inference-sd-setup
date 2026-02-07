8e838402eb458b2fca7c4d3e79e241727a0aa892
khangtruong-ui <khang.truong@gmail.com>
2026-02-07 14:37:07 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ DIFF_PATH=$(pip list -v | grep diffusers | awk '{print $3}')
 echo "INSTALLED DIFFUSER: $DIFF_PATH"
 
 cp pipeline_flax_stable_diffusion.py "$DIFF_PATH/diffusers/pipelines/stable_diffusion"
-mkdir -p $SAVE_DIR
-gsutil cp $GCS_DIR/checklist.txt .
+mkdir -p $IMAGE_OUTPUT_SAVE_DIR
+gsutil cp $GCS_OUTPUT_SAVE_DIR/checklist.txt .
 
